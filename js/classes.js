7521316ae1fb17be33af9acb0026da6e275061b7
Miles Evenson <miles.evenson@gmail.com>
2012-02-08 00:03:11 -0800

    WIP COMMIT - working on searching logic
    TODO:
    1. finalize the search scoring logic
    
    2. write a function for the Champion class that returns the matches
       for a given set of search terms
    
    3. write a function that can sort the list of champions by:
        - name
        - release date
        - search score
        - wtfever
       this function should take a comparison function as an argument!

--- js/classes.js
+++ js/classes.js
@@ -34,6 +34,65 @@ Champion.prototype.updateSearchString = function() {
     }
 
     this.searchString = this.searchString.toLowerCase();
+    this.searchString = this.searchString.replace(/[a-zA-Z0-9]+/g, '');
+}
+
+Champion.prototype.getSearchMatchScore = function(searchTerms) {
+    var score = 0.0;
+
+    if(typeof searchTerms === 'string') {
+        searchTerms = [searchTerms];
+    }
+
+    if(!(typeof searchTerms === 'array')) {
+        return score;
+    }
+
+    // TODO: iterate through searchTerms and check against various attributes of this instance
+    // Assuming that searchTerms is just letters and numbers
+    var term = '';
+    var tag = '';
+    var ability = null;
+    var matchIndex = -1;
+    for(var r in searchTerms) {
+        term = searchTerms[r];
+
+        /* TODO: decide how to score natching champion names
+            Just return a high score right here? If the champion's
+            name matches then the user probably isn't searching for
+            anything else.
+        if(this.name.toLowerCase().indexOf(term) > -1) {
+            score += 0.5;
+        }
+        */
+
+        // TODO: why not score a champion tag the same as an ability tag
+        for(var t in this.tags) {
+            tag = this.tags[t];
+
+            if(tag.indexOf(term) > -1) {
+                score += 0.5;
+            }
+        }
+
+        for(var a in this.abilities) {
+            ability = this.abilities[a];
+
+            for(var t in ability.tags) {
+                tag = ability.tags[t];
+
+                // A perfect match is worth more than a partial match
+                matchIndex = tag.indexOf(term);
+                if(matchIndex === 0 && tag.length == term.length) {
+                    score += 1.0;
+                } else if(matchIndex > 0) {
+                    score += 0.5;
+                }
+            }
+        }
+    }
+
+    return score;
 }
 
 var AbilityMatch = function(champion, matchedAbilities) {
